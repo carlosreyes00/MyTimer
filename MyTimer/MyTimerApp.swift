450afe2b3399ecacadcb893742341acd8d14ddf2
Carlos Reyes <crrmagadan@icloud.com>
2025-05-17 13:31:08 -0400

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by Carlos Reyes on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
